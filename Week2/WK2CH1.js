5f9b16cbbc3f24a1daf65c971d537e6b6870757b
sbames259 <sbames259@gmail.com>
2019-09-25 12:08:01 -0600

    Finished ch2 and optimized ch1

--- Week2/WK2CH1.js
+++ Week2/WK2CH1.js
@@ -6,13 +6,11 @@ const scores = []
 
 function testScores(score1, score2, score3) {
 
-    scores.push(score1, score2, score3)
+    scores.push(score1, score2, score3);
 
-    scores.sort(function(a, b){return b-a})
+    scores.sort(function(a, b){return b-a});
 
     return scores
 }
 
-const S = testScores(94.5, 81.3, 98.9)
-
-console.log(S)
+console.log(testScores(94.5, 81.3, 98.9))
--- Week2/WK2CH2.js
+++ Week2/WK2CH2.js
@@ -12,12 +12,13 @@ const person1 = new personCreate("Sam", "Ames", "Brown", "Brown", 22);
 
 console.log(person1);
 
+function attributeAdd(favColor) {
+    this.favoriteColor = favColor;
+}
 
+attributeAdd.call(person1, 'red');
 
-
-
-
-
+console.log(person1)
 
 
 
