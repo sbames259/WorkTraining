38c3bb75d83ce58ddd31ad4de54432217ad5b40b
sbames259 <sbames259@gmail.com>
2019-10-11 10:05:38 -0600

    Initial Upload.

---
+++ Week4/WK4CH1_Test.js
@@ -0,0 +1,56 @@
+const assets = [
+    {
+        assetId: 'Truck-001',
+        Entity: 'Tires n Stuff',
+        totalWorkOrders: 4
+    },
+    {
+       assetId: 'Truck-002',
+       Entity: 'Tires n Stuff',
+       totalWorkOrders: 44
+   },
+   {
+       assetId: 'Truck-003',
+       Entity: 'Tires n Stuff',
+       totalWorkOrders: 18
+   },
+   {
+       assetId: 'Truck-004',
+       Entity: 'Tires n Stuff',
+       totalWorkOrders: 125
+   },
+   {
+       assetId: 'Truck-005',
+       Entity: 'Tires n Stuff',
+       totalWorkOrders: 0
+   },
+   {
+       assetId: 'Truck-006',
+       Entity: 'Tires n Stuff',
+       totalWorkOrders: 5
+   },
+   {
+       assetId: 'Van-010',
+       Entity: 'Mars or Bust',
+       totalWorkOrders: 43
+   },
+   {
+       assetId: 'Ship-0090039401920',
+       Entity: 'Mars or Bust',
+       totalWorkOrders: 2
+   }
+]
+
+
+
+
+function chopAndCount(assetsArray) {
+    const fifteenOrMore = assetsArray.filter(function(e) {
+        return e.totalWorkOrders > 15;
+    })
+
+    console.log(fifteenOrMore)
+    console.log(fifteenOrMore.length)
+}
+
+console.log(chopAndCount(assets))
