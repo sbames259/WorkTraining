e59502b0a6abf90e067c27165f88338e4611d371
sbames259 <sbames259@gmail.com>
2019-10-11 09:47:04 -0600

    Initial Upload.

---
+++ Week4/WK4CH1.js
@@ -0,0 +1,60 @@
+const assets = [
+    {
+        assetId: 'Truck-001',
+        Entity: 'Tires n Stuff',
+        totalWorkOrders: 4
+    },
+    {
+       assetId: 'Truck-002',
+       Entity: 'Tires n Stuff',
+       totalWorkOrders: 44
+   },
+   {
+       assetId: 'Truck-003',
+       Entity: 'Tires n Stuff',
+       totalWorkOrders: 18
+   },
+   {
+       assetId: 'Truck-004',
+       Entity: 'Tires n Stuff',
+       totalWorkOrders: 125
+   },
+   {
+       assetId: 'Truck-005',
+       Entity: 'Tires n Stuff',
+       totalWorkOrders: 0
+   },
+   {
+       assetId: 'Truck-006',
+       Entity: 'Tires n Stuff',
+       totalWorkOrders: 5
+   },
+   {
+       assetId: 'Van-010',
+       Entity: 'Mars or Bust',
+       totalWorkOrders: 43
+   },
+   {
+       assetId: 'Ship-0090039401920',
+       Entity: 'Mars or Bust',
+       totalWorkOrders: 2
+   }
+]
+
+
+
+
+const sortByNum = assets.sort(function(a, b) {
+    return a.totalWorkOrders - b.totalWorkOrders;
+});
+
+console.log(sortByNum);
+
+
+
+const moreThanFifteen = assets.filter(function(e) {
+    return e.totalWorkOrders > 15;
+});
+
+console.log(moreThanFifteen);
+console.log(moreThanFifteen.length);
